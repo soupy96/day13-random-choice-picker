76bc3f8806b9e34372a2561d25531954a708d67e
Michael Campbell <michael.campbell14@gmail.com>
2021-01-16 14:50:17 -0500

    selecting a random tag

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ const textarea = document.getElementById("textarea");
 textarea.focus();
 
 textarea.addEventListener('keyup', (e) => {
-    createTags(e.target.value)
+    createTags(e.target.value);
+
+    if(e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = '';
+        }, 10);
+        randomSelect();
+    }
 })
 
 function createTags(input) {
@@ -16,6 +23,43 @@ function createTags(input) {
         const tagEl = document.createElement('span');
         tagEl.classList.add('tag');
         tagEl.innerText = tag;
-        tagsEl.appendChild(tagEl)
+        tagsEl.appendChild(tagEl);
     });
+}
+
+function randomSelect() {
+    const times = 30;
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+
+        highlightTag(randomTag);
+
+        setTimeout(() => {
+            unHighlightTag(randomTag);
+        }, 100);
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+
+            highlightTag(randomTag);
+        }, 100);
+    }, times * 100);
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag');
+    return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight');
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight');
 }
